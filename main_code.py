82ff8f11837acaf7503099ec60e91d324eaff594
landrewwray <106675303+landrewwray@users.noreply.github.com>
2022-06-27 14:19:49 -0400

    Add files via upload

--- main_code.py
+++ main_code.py
@@ -6,12 +6,15 @@ Created on Tue Jun 14 07:03:08 2022
 
 """
 import universalmodel.universalmodel as um
-import load_create_mol as lcm #***
-# import hmat_stitcher.hmat_stitcher as stitch
+
+import load_create_mol as lcm
+import hmat_stitcher.hmat_stitcher as stitch
 
 # 1. code to load and create molecule --> molConfigList
-atomsLists, bondsArrays, coordsArrays = lcm.load_mol.loadMol("C:/Users/danie/OneDrive/Documents/GitHub/hop-field/Molecular Structure Data/*.mol2") 
-configsLists = lcm.config_wrapper.allMolDistortions("C:/Users/danie/OneDrive/Documents/GitHub/hop-field/Molecular Structure Data/*.mol2")
+molFilePath="Molecular Structure Data/*.mol2"
+bondsPerMol=5 # Number of bonds to distort in each molecule (if enough bonds exist)
+atomsLists, bondsArrays, coordsArrays = lcm.load_mol.loadMol(molFilePath) 
+configsLists, molNumList = lcm.config_wrapper.allMolDistortions(molFilePath,bondsPerMol)
 elementsLists = lcm.config_wrapper.elementsLists(atomsLists)
 configs = lcm.config_wrapper.ConfigWrapper(configsLists, atomsLists, elementsLists, bondsArrays, coordsArrays) # distortLists, atomsLists, elementsLists, bondsArrays, coordsArrays
 
@@ -21,7 +24,8 @@ theUM += um.UniversalModel(['H','Hinit_0.txt'])
 theUM.makeCrossTerms(['all'],'Hop_generic0.txt')
 
 # #3. create the matrices
-# configMats=stitch.ConfigTerms(theUM,molConfigList)
+configMats=stitch.ConfigTerms(theUM,configs)
+
 # configMats.save('trial1')  #also need configMats.load(fileName)
 # configMats.makeHmats(theUM)  #create the Hmats for a parthticular UM
 
